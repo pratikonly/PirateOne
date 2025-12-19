a7bd7089a9743c192c0408939a684c1877e6c932
pratikonly <ashishkumarsinghx@gmail.com>
2025-12-19 12:59:30 +0000

    check

--- pirateone_backend/settings.py
+++ pirateone_backend/settings.py
@@ -94,7 +94,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static', 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'css'),
     os.path.join(BASE_DIR, 'js'),
