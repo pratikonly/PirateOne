1ab191fafdad9503705d2624c47a1f7c41cc88e9
pratikonly <ashishkumarsinghx@gmail.com>
2025-12-19 12:13:34 +0000

    .

--- pirateone_backend/wsgi.py
+++ pirateone_backend/wsgi.py
@@ -4,3 +4,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pirateone_backend.settings')
 
 application = get_wsgi_application()
+
+# Vercel now requires one of these two
+handler = application   # This satisfies Vercel's detection
+# app = application    # You can use this instead if you prefer
