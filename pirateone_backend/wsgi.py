454efa0421e63cc695d319983bae148ccc44d457
pratikonly <ashishkumarsinghx@gmail.com>
2025-12-20 07:40:42 +0000

    a

--- pirateone_backend/wsgi.py
+++ pirateone_backend/wsgi.py
@@ -4,5 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pirateone_backend.settings')
 
 application = get_wsgi_application()
-app = application  # ← This is what Vercel expects for WSGI (Flask/Django)
-# Do NOT add "handler = application" — it triggers the issubclass error!
+app = application  
