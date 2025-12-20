46d1aa248453eefc9bc15813f929571aad58af20
pratikonly <ashishkumarsinghx@gmail.com>
2025-12-20 04:35:28 +0000

    Lee t

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Installing dependencies..."
 pip install -r requirements.txt
-
-echo "Running migrations..."
 python manage.py migrate --noinput
-
-echo "Collecting static files (detailed)..."
-python manage.py collectstatic --noinput --clear --verbosity 3
+python manage.py collectstatic --noinput --clear
