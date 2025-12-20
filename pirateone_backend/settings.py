17cc62c113f8700c230a5ef20698de60fa4d1af3
pratikonly <ashishkumarsinghx@gmail.com>
2025-12-20 11:44:46 +0000

    h

--- pirateone_backend/settings.py
+++ pirateone_backend/settings.py
@@ -94,11 +94,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = []
+
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
