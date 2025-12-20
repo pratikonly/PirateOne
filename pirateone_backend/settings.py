9c5f251e0f04ffb45cc114d46cf7fe506a641c14
pratikonly <ashishkumarsinghx@gmail.com>
2025-12-20 07:31:10 +0000

    Fix static files for Vercel + WhiteNoise

--- pirateone_backend/settings.py
+++ pirateone_backend/settings.py
@@ -94,14 +94,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
